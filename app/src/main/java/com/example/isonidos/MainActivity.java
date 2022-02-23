54787cb726d064257433048b68bc0b67ffdfa6c5
Usuario <Usuario@DAVID>
2022-02-23 17:41:15 +0100

    añadido Pantalla para vídeo y vídeo1

--- app/src/main/java/com/example/isonidos/MainActivity.java
+++ app/src/main/java/com/example/isonidos/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.isonidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         int idSonido = R.raw.sonidos_directedby;
         m = MediaPlayer.create(this, idSonido);
         m.start();
+    }
+    public void reproduceVideo(View vista){
+        VideoView videoView = findViewById(R.id.videoView);
+        int idVideo = this.getResources().getIdentifier(vista.getTag().toString(), "raw", this.getPackageName());
+        Uri uri = Uri.parse("android:resource://" + getPackageName() + "/" + idVideo);
+        videoView.setVideoURI(uri);
+        videoView.start();
     }
 
 
